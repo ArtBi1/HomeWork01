293bc17d65b20632c3e4ab96b207e44898524326
ArtB <89324748589@mail.ru>
2023-03-24 19:15:16 +0500

    Работает, выдает макс

---
+++ Task02/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.Write("a:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b:");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine(a);
+}else
+{
+    Console.WriteLine(b);
+}
+
