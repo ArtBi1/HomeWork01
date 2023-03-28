e76c43d900934e4cd533be62c680c2f26c4ce71a
ArtB <89324748589@mail.ru>
2023-03-28 05:54:44 +0500

    Теперь работают и минусовые числа

--- HomeWork02/Task10/Program.cs
+++ HomeWork02/Task10/Program.cs
@@ -4,18 +4,12 @@ Console.Clear();
 Console.Write("Введите трехзначное число:");
 int a = int.Parse(Console.ReadLine());
 
-if ((a >= 100 && a <= 999))
-
+if  ((a >= 100 && a <= 999) || (a <= -100 && a >= -999))
 {
-    int b = (a / 10)%10;
+    int b = a / 10 % 10;
     
     Console.Write($"Вторая цифра у числа {a} является цифрой {b}");
-}
-
-    else{
+}else
+{
     Console.Write($"Вы ввели не трехзначное число");
-    
-
-}
-
-    
+    }
