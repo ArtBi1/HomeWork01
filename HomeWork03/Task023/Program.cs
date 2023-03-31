fbae253d27639202c84998b2b4d943b38b711da2
ArtB <89324748589@mail.ru>
2023-04-01 02:26:18 +0500

    Решена задача 23

---
+++ HomeWork03/Task023/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.Clear();
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+int[] result = new int[N];
+
+for (int i = 1; i <= N; i++)
+result[i - 1] = i * i * i;
+
+for (int i = 0; i < N; i++)
+Console.Write($" {result [i] } ");
