cdfcc9938371bb779b6feaeb43bd8aba7ba94bdb
ArtB <89324748589@mail.ru>
2023-04-09 04:41:06 +0500

    Решена задача 25

--- HomeWork04/Task25/Program.cs
+++ HomeWork04/Task25/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+
+int num = a;
+
+for (int i = 1; i < b; i++ )
+{
+    num = num * a;
+
+}
+
+Console.WriteLine($"{a} в степени {b} равняется {num}");
