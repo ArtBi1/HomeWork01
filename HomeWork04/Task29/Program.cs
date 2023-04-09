b249f2bdf1d340d533f4d6f11f8cdf168663b9ec
ArtB <89324748589@mail.ru>
2023-04-09 05:01:25 +0500

    Решена задача 29

--- HomeWork04/Task29/Program.cs
+++ HomeWork04/Task29/Program.cs
@@ -2,16 +2,14 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-
-int[] num = GetBinaryArray(8);
-
-Console.WriteLine($"[{String.Join(", ", array)}]");
-
-int[] GetBinaryArray ( int size ){
+int[] array = GetArray(8);
+int[] GetArray ( int size ){
     int[] result = new int [size];
     for ( int i = 0; i < size; i++)
     {
-        result[i] = new Random().Next(2);
+        result[i] = new Random().Next(100);
     }
     return result;
 }
+
+Console.WriteLine($"[{String.Join(", ", array)}]");
