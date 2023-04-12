4756bbe63cac219de47d3e2be21b86e73f1967a8
ArtB <89324748589@mail.ru>
2023-04-12 19:55:36 +0500

    Решена задача 36

--- HomeWork05/Task036/Program.cs
+++ HomeWork05/Task036/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+Console.Clear();
+
+int[] nums = new int[10];
+RandomNumbers(nums);
+Console.WriteLine("Одномерный массив, заполненный случайными числами: ");
+Print(nums);
+int sum = 0;
+for (int i = 0; i < nums.Length; i+=2)
+        sum = sum + nums[i];
+        Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях равна {sum}");
+
+void RandomNumbers(int[] nums)
+{
+    for(int i = 0; i < nums.Length; i++ )
+    
+        nums[i] = new Random().Next(-100,100); // Здесь можно задать нужный промежуток, среди которого будут генерироваться рандомные числа
+        }
+    
+void Print(int[] nums)
+{
+    for(int i = 0; i < nums.Length; i++)
+    {
+        Console.Write(nums[i] + " ");
+    }
+             Console.WriteLine();
+}
