c3d917a09f0ebf66e2e46095081b4245bf34f69d
ArtB <89324748589@mail.ru>
2023-04-13 06:20:33 +0500

    Решена задача 38

--- HomeWork05/Task038/Program.cs
+++ HomeWork05/Task038/Program.cs
@@ -3,45 +3,52 @@
 
 
 Console.Clear();
-Console.WriteLine("Введите размер массива");
+Console.WriteLine("Введите размер массива:");
 int a = int.Parse(Console.ReadLine());
-double[] numbers = new double[10];
-FillArrayRandomNumbers(numbers);
-Console.WriteLine("Массив вещественных чисел: ");
-PrintArray(numbers);
-Console.WriteLine(String.Join (" ", numbers));
+int[] MyArray = RandomArray (a, -100, 100);
+int sum = 0;
+Console.WriteLine(String.Join(" ", MyArray));
+Console.WriteLine($"Максимальное число массива {Maximum(MyArray)}, минимальное число {Minimum(MyArray)}, разница между ними = {Maximum(MyArray) - Minimum(MyArray)}");
 
-int minNumber = numbers[0];
-int maxNumber = numbers[0];
 
-MinNumber (numbers);
-int MinNumber (int []numbers) {
-    int res = minNumber;
-    for (int i = 0; i < numbers.Length; i++) {
-        if (numbers[i] < minNumber) {
-            minNumber = numbers[i];
+int[] RandomArray (int size, int minValue, int maxValue)
+{
+    int[] a = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        a[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return a;
+}
+
+int Maximum(int[] array)
+{
+    int max = int.MinValue;
+    if (MyArray.Length != 0)
+    {
+        foreach (var i in MyArray)
+        {
+            if (i > max)
+            {
+                max = i;
+            }
         }
     }
-    return res;
+    return max;
 }
 
-MaxNumber (numbers);
-int MaxNumber (int []numbers) {
-    int re = maxNumber;
-    for (int i = 0; i < numbers.Length; i++) {
-        if (numbers[i] > maxNumber) {
-            maxNumber = numbers[i];
+int Minimum(int[] array)
+{
+    int min = int.MaxValue;
+    if (MyArray.Length != 0)
+    {
+        foreach (var i in MyArray)
+        {
+            if (i < min)
+            {
+                min = i;
+            }
         }
     }
-    return re;
+        return min;
 }
-
-
-DiffNumbers (numbers);
-int DiffNumbers (int[] numbers) {
-    int result = maxNumber - minNumber;
-    for (int i = 0; i < numbers.Length; i++) {
-    }
-    return result;
-}
-Console.WriteLine($"Разница между макс и мин значением равна {DiffNumbers(numbers)}"); 
