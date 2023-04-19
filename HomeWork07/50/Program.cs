bd2f8c85208389723fc9d56222d6a0f21e72ee59
ArtB <89324748589@mail.ru>
2023-04-20 04:53:37 +0500

    Решены задачи 47;50;52

---
+++ HomeWork07/50/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк массива: ");
+int userRows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива: ");
+int userColumns = int.Parse(Console.ReadLine());
+
+double[,] RandomArray = new double[userRows,userColumns];
+
+Console.WriteLine("Введите позицию на строке массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите позицию на столбце массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+void rand (int a, int b)
+{
+    int i, j;
+    Random RandomNums = new Random();
+    for (i = 0; i < a; i++)
+    {
+        Console.WriteLine();
+        for (j = 0; j < b; j++)
+    {
+        RandomArray[i,j] = RandomNums.NextDouble();
+        Console.Write($"{RandomArray[i,j]:F2} "); // Console.Write($"{RandomArray[i,j]F:5} "); даст остаток 5 в элементах массива
+    }
+    Console.WriteLine();
+}
+}
+
+rand (userRows,userColumns);
+if (rows < 1 || columns < 1)
+Console.WriteLine("Не правильная позиция строки");
+else if (rows <= userRows+1 && columns <= userColumns+1)
+Console.Write($"Значение выбранного элемента равно {RandomArray[columns-1,rows-1]:F2} "); // Console.Write($"Значение элемента равно {RandomArray[columns-1,rows-1]:F5} "); даст остаток 5 в значении
+else Console.WriteLine("Такого элемента нет в массиве");
+
+
+
---
+++ HomeWork07/52/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+Console.WriteLine("Введите количество строк массива:");
+int numRows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива:");
+int numColumns = int.Parse(Console.ReadLine());
+
+int[,] numbers = new int[numRows, numColumns];
+RandomNumbers (numbers);
+
+Console.WriteLine("Среднее арефметическое столбцов:");
+for (int i = 0; i < numbers.GetLength(1); i++)
+{
+    double mean = 0;
+    for (int j = 0; j < numbers.GetLength(0); j++)
+    {
+        mean = (mean + numbers[j, i]);
+    }
+    mean = mean / numRows;
+    
+    Console.Write(mean + "; ");
+}
+Console.WriteLine();
+Console.WriteLine("В массиве:");
+Print(numbers);
+
+void RandomNumbers(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void Print(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+       
+        Console.WriteLine("");
+    }
+}
